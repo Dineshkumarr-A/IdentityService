8cb8fb0f12d4ba7a18c3771566787e9ff6860b53
Dineshkumar <dinesh.athiappan@outlook.com>
2025-08-30 13:50:55 +0530

    Add project files.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityService.Controllers
+{
+    public class AuthController : Controller
+    {
+        // TODO: Implement authentication logic here
+    }
+}
